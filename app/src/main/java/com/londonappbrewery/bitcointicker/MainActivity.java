7073ff1d46831d73fc03faf785b39c30f0caf9c1
Artem <captainhorsepower228@gmai.com>
2018-01-27 13:51:16 +0300

    That was Really time consuming!

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -1,6 +1,9 @@
 package com.londonappbrewery.bitcointicker;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -12,16 +15,22 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
 
     // Member Variables:
     TextView mPriceTextView;
@@ -34,22 +43,69 @@ public class MainActivity extends AppCompatActivity {
         mPriceTextView = (TextView) findViewById(R.id.priceLabel);
         Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
 
-        // Create an ArrayAdapter using the String array and a spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.currency_array, R.layout.spinner_item);
 
-        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
 
-        // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
-        // TODO: Set an OnItemSelected listener on the spinner
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+
+                Log.d("Bit", "selecred currency = " + parent.getItemAtPosition(position));
+
+                String url = BASE_URL + (String) parent.getItemAtPosition(position);
+
+                letsDoSomeNetworking(url);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+                Log.d("Bit", "onNothingSelected() callback recieved");
+
+            }
+        });
     }
 
     // TODO: complete the letsDoSomeNetworking() method
-    private void letsDoSomeNetworking(String url) {
+    private void letsDoSomeNetworking(String FINAL_URL) {
+
+        AsyncHttpClient bitClient = new AsyncHttpClient();
+
+        bitClient.get(FINAL_URL, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+
+                Log.d("Bit", "Response " + String.valueOf(response));
+
+                String price = null;
+                try {
+                    price = response.getJSONObject("averages").getString("day");
+                    Log.d("Bit","Price = " + price);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.d("Bit", "JSONExeption e");
+                }
+
+                mPriceTextView.setText(price);
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
+
+                Log.d("Bit", "Failure with statusCode = " + statusCode);
+            }
+        });
+    }
+
+    //private void letsDoSomeNetworking(String url){
 
 //        AsyncHttpClient client = new AsyncHttpClient();
 //        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
@@ -72,8 +128,6 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
-
-    }
 
 
 }
